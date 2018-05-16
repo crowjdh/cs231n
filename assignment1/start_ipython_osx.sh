09d7df855f27aac78137b3843c04fc0883a5424b
ChrisJeong <crowjdh@gmail.com>
2018-05-16 18:41:09 +0900

    Implementing tensorflow conv net

--- assignment1/start_ipython_osx.sh
+++ assignment1/start_ipython_osx.sh
@@ -1,5 +1,5 @@
 # Assume the virtualenv is called .env
 
 cp frameworkpython .env/bin
-.env/bin/frameworkpython -m IPython notebook
+.env/bin/frameworkpython -m IPython notebook --port=8889
 # .env/bin/frameworkpython -m ipython notebook
